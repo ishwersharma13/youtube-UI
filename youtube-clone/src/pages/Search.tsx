9f01ae7e993906b3423cc77104fd467867352f54
ishwersharma13 <ishwersharma@gmail.com>
2023-05-15 12:41:11 +0530

    final

--- youtube-clone/src/pages/Search.tsx
+++ youtube-clone/src/pages/Search.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Navbar } from '../components/Navbar'
+import { Sidebar } from '../components/Sidebar'
+import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { getHomePageVideos } from '../store/reducers/getHomePageVideos'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Spinner from '../components/Spinner'
+import { HomePageVideos } from '../Types'
+import { clearVideos } from '../store'
+import { useNavigate } from 'react-router-dom'
+import { getSearchPageVideos } from '../store/reducers/getSearchPageVideos'
+import SearchCard from '../components/SearchCard'
 
 export const Search = () => {
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+
+  useEffect(() => {
+    dispatch(clearVideos());
+    if (searchTerm === "") navigate("/");
+    else {
+      dispatch(getSearchPageVideos(false));
+    }
+  }, [dispatch, navigate, searchTerm]);
   return (
-    <div>Search</div>
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: '7.5vh' }} className="">
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <div className="py-8 pl-8 flex flex-col gap-5 w-full">
+            <InfiniteScroll
+              dataLength={videos.length}
+              next={() => dispatch(getSearchPageVideos(true))}
+              hasMore={videos.length < 500}
+              loader={<Spinner />}
+              height={600}
+            >
+              {videos.map((item: HomePageVideos) => {
+                return (
+                  <div className="my-5">
+                    <SearchCard data={item} key={item.videoId} />
+                  </div>
+                );
+              })}
+            </InfiniteScroll>
+          </div>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
   )
 }
